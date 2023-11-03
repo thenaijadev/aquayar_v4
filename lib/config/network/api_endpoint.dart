19b2235debcc4d16c0a9250db18861bd9372185d
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2023-11-03 21:54:51 +0100

    Update api_endpoint.dart

--- lib/config/network/api_endpoint.dart
+++ lib/config/network/api_endpoint.dart
@@ -1,29 +1,29 @@
 class RoutesAndPaths {
   //Base URL
-  static const String baseUrl = "https://aquayar.up.railway.app";
+  static const String baseUrl = "BASEURL";
 
 //google
-  static const String googleAuthSignUp = "/api/auth/sign-up/google/";
-  static const String googleAuthSignIn = "/api/auth/sign-in/google/";
+  static const String googleAuthSignUp = "ROUTE";
+  static const String googleAuthSignIn = "ROUTE";
 
 //User
-  static const String user = "/api/users/me";
-  static const String checkOtp = "/api/users/me/check-otp/";
-  static const String verifyPhone = "/api/users/me/verify-phone/";
-  static const String location = "/api/customers/me/locations";
-  static const String getOtp = "/api/users/me/verify-phone";
-  static const String userOrders = "/api/users/me/orders";
+  static const String user = "ROUTE";
+  static const String checkOtp = "ROUTE";
+  static const String verifyPhone = "ROUTE";
+  static const String location = "ROUTE";
+  static const String getOtp = "ROUTE";
+  static const String userOrders = "ROUTE";
 
 //auth
-  static const String authSignUp = "/api/auth/sign-up/";
-  static const String authSignIn = "/api/auth/sign-in/";
+  static const String authSignUp = "ROUTE";
+  static const String authSignIn = "ROUTE";
 
-  static const String forgotPassword = "/api/auth/forgot-password/";
-  static const String verifyOtpToChangePassword = "/api/auth/verify-otp";
-  static const String resetPassword = "/api/auth/reset-password/";
+  static const String forgotPassword = "ROUTE";
+  static const String verifyOtpToChangePassword = "ROUTE";
+  static const String resetPassword = "ROUTE";
 
 //Orders
-  static const String orderInit = "/api/orders/drivers";
-  static const String getPrice = "/api/orders/price";
-  static const String createOrder = "/api/orders";
+  static const String orderInit = "ROUTE";
+  static const String getPrice = "ROUTE";
+  static const String createOrder = "ROUTE";
 }
