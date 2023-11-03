6a6f6c5230f48fefa399e47ee5545d991a0abf5b
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2023-11-03 21:55:38 +0100

    Update super_base_provoder_impl.dart

--- lib/config/superbase/provider/super_base_provoder_impl.dart
+++ lib/config/superbase/provider/super_base_provoder_impl.dart
@@ -5,9 +5,9 @@ class SuperBaseProviderImpl implements SuperBase {
   @override
   Future<void> superBaseInit() async {
     await Supabase.initialize(
-      url: 'https://mwukqrbysdwiyonnjfoi.supabase.co',
+      url: 'URL',
       anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13dWtxcmJ5c2R3aXlvbm5qZm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI0NDQyMDgsImV4cCI6MjAwODAyMDIwOH0.dscUW1smbhRJUs13-1jbw921WLtvMjK7d_vadyZxJ1w',
+          'ANNON KEY FROM SUPERBASE',
     );
   }
 }
